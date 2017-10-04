0c54cf2d745f6bc5f4f0dd18b4f4b2bfcd017f28
MartinaMasto <martina.masto@thinkopen.it>
2017-10-04 13:00:27 +0200

    First project commit

---
+++ htdocs/app/code/local/Masto/Helloworld/Helper/Data.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Hello World...*/
+
+/**
+ * Hello World
+ * 
+ * Main Helper
+ * @author Martina Masto <martina.masto@thinkopen.it>
+ * @package Frontend
+ * @version 0.1.0
+ */
+class Masto_Helloworld_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    /**
+     * getConfig
+     * 
+     * @param string $config
+     * @return string
+     */
+    public function getConfig($config)
+    {
+        return Mage::getStoreConfig('masto_helloworld/' . $config);
+    }
+    
+    /**
+     * isEnabled
+     * 
+     * Returns true ifi this module is enabled.
+     * @return bool
+     */
+    public function isEnabled()
+    {
+        return $this->getConfig('configuration/enabled');
+    }
+}
---
+++ htdocs/app/code/local/Masto/Helloworld/controllers/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Hello World...*/
+
+/**
+ * Hello World
+ * 
+ * Hello World Index Controller
+ * @author Martina Masto <martina.masto@thinkopen.it>
+ * @package Frontend
+ * @version 0.1.0
+ */
+class Masto_Helloworld_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+        echo'hello';
+    }
+}
