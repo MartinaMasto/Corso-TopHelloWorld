6963ee7281685c8e3233d6fe2eaadac06a125606
MartinaMasto <martina.masto@thinkopen.it>
2017-10-05 18:01:18 +0200

    commit for IndexController.php

--- htdocs/app/code/local/Masto/Helloworld/controllers/IndexController.php
+++ htdocs/app/code/local/Masto/Helloworld/controllers/IndexController.php
@@ -14,6 +14,7 @@ class Masto_Helloworld_IndexController extends Mage_Core_Controller_Front_Action
 {
     public function indexAction()
     {
-        echo'hello';
+        $this->loadLayout();
+        $this->renderLayout();
     }
 }
